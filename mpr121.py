7441727d54a6dcd8e64fb2f27f45b31b8d927041
PenguinTwin2 <tristankanitz@gmail.com>
2026-04-29 20:14:53 -0400

    Add 2 pad capability

--- mpr121.py
+++ mpr121.py
@@ -8,10 +8,13 @@ from pythonosc.udp_client import SimpleUDPClient
 
 # SuperCollider OSC target
 SC_IP = "127.0.0.1"
-SC_PORT = 57120
+SC_PORT = 57121
 
 # Which MPR121 electrode to watch
-ELECTRODE = 0
+pin0 = 0
+pin1 = 1
+pin2 = 2
+pin3 = 3
 
 # Create OSC client to SuperCollider
 sc = SimpleUDPClient(SC_IP, SC_PORT)
@@ -25,24 +28,60 @@ sc = SimpleUDPClient(SC_IP, SC_PORT)
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = MPR121(i2c)
 
-last_state = False
+last_state_0 = False
+last_state_1 = False
+last_state_2 = False
+last_state_3 = False
 
-print(f"Watching electrode E{ELECTRODE}...")
+print(f"Watching pins 0 1 2 3...")
 print(f"Sending OSC to {SC_IP}:{SC_PORT}")
 
 while True:
     try:
-        touched = mpr121[ELECTRODE].value
+        touched0 = mpr121[pin0].value
+        touched1 = mpr121[pin1].value
+        touched2 = mpr121[pin2].value
+        touched3 = mpr121[pin3].value
 
-        if touched != last_state:
-            if touched:
-                print("Touched")
-                sc.send_message("/gate", 0)
+        if touched0 != last_state_0:
+            if touched0:
+                print("Pin 0 Touched")
+                sc.send_message("/pad", [0, 0, 1])
             else:
-                print("Released")
-                sc.send_message("/gate", 1)
+                print("Pin 0 Released")
+                sc.send_message("/pad", [0, 0, 0])
 
-            last_state = touched
+            last_state_0 = touched0
+            
+        if touched1 != last_state_1:
+            if touched1:
+                print("Pin 1 Touched")
+                sc.send_message("/pad", [0, 1, 1])
+            else:
+                print("Pin 1 Released")
+                sc.send_message("/pad", [0, 1, 0])
+
+            last_state_1 = touched1
+            
+        if touched2 != last_state_2:
+            if touched2:
+                print("Pin 2 Touched")
+                sc.send_message("/pad", [1, 0, 1])
+            else:
+                print("Pin 2 Released")
+                sc.send_message("/pad", [1, 0, 0])
+
+            last_state_2 = touched2
+
+        if touched3 != last_state_3:
+            if touched3:
+                print("Pin 3 Touched")
+                sc.send_message("/pad", [1, 1, 1])
+            else:
+                print("Pin 3 Released")
+                sc.send_message("/pad", [1, 1, 0])
+
+            last_state_3 = touched3
 
         time.sleep(0.01)
 

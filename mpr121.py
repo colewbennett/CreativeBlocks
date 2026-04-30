6e1067001847a06932f909f98363897257a23a71
Cole Bennett <cwbenn26@colby.edu>
2026-04-29 20:20:54 -0400

    Update mpr121.py

--- mpr121.py
+++ mpr121.py
@@ -1,90 +1,87 @@
 import time
 import board
-import RPi.GPIO as GPIO
 import busio
 
 from adafruit_mpr121 import MPR121
 from pythonosc.udp_client import SimpleUDPClient
 
-# SuperCollider OSC target
+# =====================================================
+# SUPERCOLLIDER OSC TARGET
+# =====================================================
+
 SC_IP = "127.0.0.1"
 SC_PORT = 57121
 
-# Which MPR121 electrode to watch
-pin0 = 0
-pin1 = 1
-pin2 = 2
-pin3 = 3
-
-# Create OSC client to SuperCollider
 sc = SimpleUDPClient(SC_IP, SC_PORT)
 
-#GPIO.setmode(GPIO.BCM)
-#SCL_PIN = 9
-#SDA_PIN = 8
-#GPIO.setup(SCL_PIN, GPIO.IN)
-# Set up I2C and MPR121
-#i2c = busio.I2C(1, 0)
+
+# =====================================================
+# MPR121 SETUP
+# =====================================================
+
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = MPR121(i2c)
 
-last_state_0 = False
-last_state_1 = False
-last_state_2 = False
-last_state_3 = False
+# We are using MPR slots 0 through 5:
+#
+# Pad 0 / Song 1 = slots 0 and 1
+# Pad 1 / Song 2 = slots 2 and 3
+# Pad 2 / Song 3 = slots 4 and 5
+#
+# SuperCollider decides:
+# left only  = vocals
+# right only = melody + bass
+# both       = drums
 
-print(f"Watching pins 0 1 2 3...")
+WATCHED_PINS = [0, 1, 2, 3, 4, 5]
+
+last_states = {
+    pin: False for pin in WATCHED_PINS
+}
+
+
+# =====================================================
+# STARTUP PRINTS
+# =====================================================
+
+print("====================================================")
+print("CREATIVEBLOCKS MPR121 PYTHON SENDER")
+print("====================================================")
+print("Watching MPR121 pins 0, 1, 2, 3, 4, 5")
+print("")
+print("Pad 0 / Song 1 = pins 0 and 1")
+print("Pad 1 / Song 2 = pins 2 and 3")
+print("Pad 2 / Song 3 = pins 4 and 5")
+print("")
+print("Sending OSC messages in this format:")
+print("/mpr slot on")
+print("")
 print(f"Sending OSC to {SC_IP}:{SC_PORT}")
+print("====================================================")
+
+
+# =====================================================
+# MAIN LOOP
+# =====================================================
 
 while True:
     try:
-        touched0 = mpr121[pin0].value
-        touched1 = mpr121[pin1].value
-        touched2 = mpr121[pin2].value
-        touched3 = mpr121[pin3].value
+        for pin in WATCHED_PINS:
+            touched = mpr121[pin].value
 
-        if touched0 != last_state_0:
-            if touched0:
-                print("Pin 0 Touched")
-                sc.send_message("/pad", [0, 0, 1])
-            else:
-                print("Pin 0 Released")
-                sc.send_message("/pad", [0, 0, 0])
+            if touched != last_states[pin]:
+                if touched:
+                    print(f"Pin {pin} Touched")
+                    sc.send_message("/mpr", [pin, 1])
+                else:
+                    print(f"Pin {pin} Released")
+                    sc.send_message("/mpr", [pin, 0])
 
-            last_state_0 = touched0
-            
-        if touched1 != last_state_1:
-            if touched1:
-                print("Pin 1 Touched")
-                sc.send_message("/pad", [0, 1, 1])
-            else:
-                print("Pin 1 Released")
-                sc.send_message("/pad", [0, 1, 0])
-
-            last_state_1 = touched1
-            
-        if touched2 != last_state_2:
-            if touched2:
-                print("Pin 2 Touched")
-                sc.send_message("/pad", [1, 0, 1])
-            else:
-                print("Pin 2 Released")
-                sc.send_message("/pad", [1, 0, 0])
-
-            last_state_2 = touched2
-
-        if touched3 != last_state_3:
-            if touched3:
-                print("Pin 3 Touched")
-                sc.send_message("/pad", [1, 1, 1])
-            else:
-                print("Pin 3 Released")
-                sc.send_message("/pad", [1, 1, 0])
-
-            last_state_3 = touched3
+                last_states[pin] = touched
 
         time.sleep(0.01)
 
     except KeyboardInterrupt:
         print("\nExiting.")
+        sc.send_message("/stopall", [1])
         break
